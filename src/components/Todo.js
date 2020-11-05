7dfff8872e0580b079dcc7f6ffe6af2d35806f42
SharifAktar <sharifzaman@ymail.com>
2020-11-05 22:59:54 +0100

    init

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Todo(props)  {
+    
+    return (
+
+        <div>
+           <h1> My name is {props.name}  and  I'm  {props.age} 
+           years old and I live in {props.place}</h1> 
+        </div>
+    )
+    };
+
+export default Todo;
+
+
